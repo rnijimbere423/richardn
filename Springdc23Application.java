d64f087f4df4445c33210d83f3b9dc9c6be71aef
myweb <87034663+rnijimbere423@users.noreply.github.com>
2022-06-07 09:57:59 -0700

    Add files via upload

---
+++ Springdc23Application.java
@@ -0,0 +1,14 @@
+
+package com.example.springdc23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springdc23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springdc23Application.class, args);
+    }
+
+}
